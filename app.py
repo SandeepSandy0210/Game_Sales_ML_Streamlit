0df569186975159bfd4c02f87bb2111430458719
Sai Sandeep <79784447+AlphaHeart02@users.noreply.github.com>
2023-12-06 11:38:20 +0530

    Update app.py
    modified y-axis label to in million

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
 
     selected_y_column = st.sidebar.selectbox(
         "Select Y-Axis Column",
-        ["Global_Sales", "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]
+        ["Global_Sales_in_Millions", "NA_Sales_in_Million", "EU_Sales_in_Millions", "JP_Sales_in_Millions", "Other_Sales_in_Millions"]
     )
 
     # Filter data based on selected columns
